7300ab745daf5e9095e93083792ce422afcf892c
Imene Fadhlaoui <imenefadhlaoui19@gmail.com>
2023-09-08 19:52:06 +0200

    Mise en place du Header et du Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
-import React from "react";
-
+import styles from "./Footer.module.scss"
 function Footer() {
-  return <div className="b2">Footer</div>;
+  return (
+  <footer className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}>
+    <p>Copyright 2023 Recette PinguCoder,Inc.</p>
+  </footer>
+  )
 }
 
 export default Footer;
